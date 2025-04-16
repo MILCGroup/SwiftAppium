cdcd804d0838ec220453d9814fd2867aab8789d6
Dalton Alexandre <166029845+dl-alexandre@users.noreply.github.com>
2025-04-15 22:47:35 -0700

    public init

--- Sources/Element.swift
+++ Sources/Element.swift
@@ -10,8 +10,17 @@ import Foundation
 public struct Element {
     public let strategy: Strategy
     public let selector: Selector
+    
+    public init(_ strategy: Strategy,_ selector: Selector) {
+        self.strategy = strategy
+        self.selector = selector
+    }
 }
 
 public struct Selector {
     public let wrappedValue: String
+    
+    public init(_ wrappedValue: String) {
+        self.wrappedValue = wrappedValue
+    }
 }
