eabb6e7606fe5c1b28728652388f2823312c25d9
Dalton Alexandre <166029845+dl-alexandre@users.noreply.github.com>
2025-04-24 09:07:24 -0700

    Add Session Funcs

--- Sources/Core/Session.swift
+++ Sources/Core/Session.swift
@@ -326,4 +326,240 @@ public struct Session: Sendable {
         
         return false
     }
+    
+    public func isChecked(
+        _ element: Element
+    ) async throws -> Bool {
+        appiumLogger.info(
+            "Checking visibility of element with strategy: \(element.strategy.rawValue) and selector: \(element.selector.wrappedValue) in session: \(id)"
+        )
+
+        let elementId: String
+        do {
+            elementId = try await select(element)
+        } catch {
+            appiumLogger.error("Failed to find element: \(error)")
+            throw error
+        }
+
+        let request = try HTTPClient.Request(
+            url: API.displayed(elementId, id).path,
+            method: .GET
+        )
+
+        appiumLogger.info("Sending request to URL: \(request.url)")
+        
+        let response: HTTPClient.Response
+        do {
+            response = try await client.execute(request: request).get()
+        } catch {
+            appiumLogger.error("Failed to execute request: \(error)")
+            throw error
+        }
+
+        guard response.status == .ok else {
+            appiumLogger.error("Failed to check element visibility: HTTP \(response.status)")
+            throw AppiumError.invalidResponse(
+                "Failed to check element visibility: HTTP \(response.status)"
+            )
+        }
+
+        guard let responseData = response.body else {
+            appiumLogger.error("No response body")
+            throw AppiumError.invalidResponse(
+                "No response data received when checking element visibility."
+            )
+        }
+
+        if let responseString = responseData.getString(
+            at: 0,
+            length: responseData.readableBytes
+        ) {
+            appiumLogger.info("Raw response data: \(responseString)")
+        }
+
+        do {
+            let checkedResponse = try JSONDecoder().decode(
+                CheckedResponse.self,
+                from: responseData
+            )
+            return checkedResponse.value
+        } catch {
+            appiumLogger.error(
+                "Failed to decode visibility response: \(error.localizedDescription)"
+            )
+            throw AppiumError.invalidResponse(
+                "Failed to decode visibility response: \(error.localizedDescription)"
+            )
+        }
+    }
+    
+    public func value(
+         _ element: Element
+    ) async throws -> Double {
+        appiumLogger.info(
+            "Checking value of element with strategy: \(element.strategy.rawValue) and selector: \(element.selector.wrappedValue) in session: \(id)"
+        )
+        
+        let elementId: String
+        do {
+            elementId = try await select(
+                element, 35)
+        } catch {
+            appiumLogger.error("Failed to find element: \(error)")
+            throw error
+        }
+        
+        let request: HTTPClient.Request
+        do {
+            request = try HTTPClient.Request(
+                url: API.attributeValue(elementId, id).path,
+                method: .GET
+            )
+        } catch {
+            appiumLogger.error("Failed to create request: \(error)")
+            throw AppiumError.invalidResponse(
+                "Failed to create request: \(error.localizedDescription)"
+            )
+        }
+        
+        appiumLogger.info("Sending request to URL: \(request.url)")
+        let response: HTTPClient.Response
+        do {
+            response = try await client.execute(request: request).get()
+        } catch {
+            appiumLogger.error("Failed to execute request: \(error)")
+            throw error
+        }
+        
+        appiumLogger.info("Received response with status: \(response.status)")
+        guard response.status == .ok else {
+            appiumLogger.error(
+                "Failed to check element value: HTTP \(response.status)"
+            )
+            throw AppiumError.invalidResponse(
+                "Failed to check element value: HTTP \(response.status)"
+            )
+        }
+        
+        guard let responseData = response.body else {
+            appiumLogger.error("No response body")
+            throw AppiumError.invalidResponse(
+                "No response data received when checking element value."
+            )
+        }
+        
+        if let responseString = responseData.getString(
+            at: 0, length: responseData.readableBytes
+        ) {
+            appiumLogger.info("Raw response data: \(responseString)")
+        } else {
+            appiumLogger.error("Failed to read response data as string")
+        }
+        
+        do {
+            let valueResponse = try JSONDecoder().decode(
+                ValueResponse.self, from: responseData
+            )
+            let valueString = valueResponse.value
+            let numericString = valueString.trimmingCharacters(in: CharacterSet(charactersIn: "0123456789.-").inverted)
+            guard var doubleValue = Double(numericString) else {
+                appiumLogger.error("Failed to convert value to Double")
+                throw AppiumError.invalidResponse(
+                    "Failed to convert value to Double"
+                )
+            }
+            if valueString.contains("%") {
+                doubleValue /= 100
+            }
+            if valueString.contains(".") {
+                let decimalPart = valueString.split(separator: ".")[1].trimmingCharacters(in: CharacterSet(charactersIn: "0123456789.-").inverted)
+                if decimalPart.count == 2 {
+                    return doubleValue + 0.01 // Carry two decimals into Tests
+                }
+            }
+            return doubleValue
+        } catch {
+            appiumLogger.error(
+                "Failed to decode value response: \(error.localizedDescription)"
+            )
+            throw AppiumError.invalidResponse(
+                "Failed to decode value response: \(error.localizedDescription)"
+            )
+        }
+    }
+    
+    public func isVisible(
+        _ element: Element
+    ) async throws -> Bool {
+        appiumLogger.info(
+            "Checking visibility of element with strategy: \(element.strategy.rawValue) and selector: \(element.selector.wrappedValue) in session: \(id)"
+        )
+        
+        let elementId: String
+        do {
+            elementId = try await select(
+                element)
+        } catch {
+            appiumLogger.error("Failed to find element: \(error)")
+            throw error
+        }
+        
+        let request: HTTPClient.Request
+        do {
+            request = try HTTPClient.Request(
+                url:
+                    API.displayed(elementId, id).path,
+                method: .GET
+            )
+        } catch {
+            appiumLogger.error("Failed to create request: \(error)")
+            throw AppiumError.invalidResponse(
+                "Failed to create request: \(error.localizedDescription)")
+        }
+        
+        appiumLogger.info("Sending request to URL: \(request.url)")
+        let response: HTTPClient.Response
+        do {
+            response = try await client.execute(request: request).get()
+        } catch {
+            appiumLogger.error("Failed to execute request: \(error)")
+            throw error
+        }
+        
+        appiumLogger.info("Received response with status: \(response.status)")
+        guard response.status == .ok else {
+            appiumLogger.error(
+                "Failed to check element visibility: HTTP \(response.status)")
+            throw AppiumError.invalidResponse(
+                "Failed to check element visibility: HTTP \(response.status)")
+        }
+        
+        guard let responseData = response.body else {
+            appiumLogger.error("No response body")
+            throw AppiumError.invalidResponse(
+                "No response data received when checking element visibility.")
+        }
+        
+        if let responseString = responseData.getString(
+            at: 0, length: responseData.readableBytes)
+        {
+            appiumLogger.info("Raw response data: \(responseString)")
+        } else {
+            appiumLogger.error("Failed to read response data as string")
+        }
+        
+        do {
+            let visibilityResponse = try JSONDecoder().decode(
+                VisibilityResponse.self, from: responseData)
+            return visibilityResponse.value
+        } catch {
+            appiumLogger.error(
+                "Failed to decode visibility response: \(error.localizedDescription)"
+            )
+            throw AppiumError.invalidResponse(
+                "Failed to decode visibility response: \(error.localizedDescription)"
+            )
+        }
+    }
 }
